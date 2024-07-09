d050c8436d05e56b9ca3daead4aba22d3cbe08a6
buzmate <marios@buzmate.com>
2024-07-09 15:34:13 +0300

    ddd

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,48 @@
 <?php namespace Webmors\SmsSender;
 
-use App;
-use Backend;
 use System\Classes\PluginBase;
-
+use App;
+use Config;
+use Illuminate\Foundation\AliasLoader;
 /**
  * SmsSender Plugin Information File
  */
 class Plugin extends PluginBase
 {
+    public function bootPackages()
+    {
+        // Get the namespace of the current plugin to use in accessing the Config of the plugin
+        $pluginNamespace = str_replace('\\', '.', strtolower(__NAMESPACE__));
+        
+        // Instantiate the AliasLoader for any aliases that will be loaded
+        $aliasLoader = AliasLoader::getInstance();
+        
+        // Get the packages to boot
+        $packages = Config::get($pluginNamespace . '::packages');
+        
+        // Boot each package
+        foreach ($packages as $name => $options) {
+            // Setup the configuration for the package, pulling from this plugin's config
+            if (!empty($options['config']) && !empty($options['config_namespace'])) {
+                Config::set($options['config_namespace'], $options['config']);
+            }
+            
+            // Register any Service Providers for the package
+            if (!empty($options['providers'])) {
+                foreach ($options['providers'] as $provider) {
+                    App::register($provider);
+                }
+            }
+            
+            // Register any Aliases for the package
+            if (!empty($options['aliases'])) {
+                foreach ($options['aliases'] as $alias => $path) {
+                    $aliasLoader->alias($alias, $path);
+                }
+            }
+        }
+    }
+    
     public function pluginDetails()
     {
         return [
@@ -42,3 +76,4 @@ class Plugin extends PluginBase
         ];
     }
 }
+
